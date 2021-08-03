e7328b8a7b9310fc285bd0364159b745754d1c24
MohammedAldouis <85803945+MohammedAldouis@users.noreply.github.com>
2021-08-03 12:13:35 +0300

    Js

---
+++ js/Js.js
@@ -0,0 +1,29 @@
+function test1(){
+    var a = document.getElementById('one');
+    if(a.style.display == 'block'){
+        a.style.display ='none';
+    }
+    else{
+        a.style.display = 'block';
+    }
+}
+
+function test2(){
+    var a = document.getElementById('two');
+    if(a.style.display == 'block'){
+        a.style.display ='none';
+    }
+    else{
+        a.style.display = 'block';
+    }
+}
+
+function test3(){
+    var a = document.getElementById('three');
+    if(a.style.display == 'block'){
+        a.style.display ='none';
+    }
+    else{
+        a.style.display = 'block';
+    }
+}
